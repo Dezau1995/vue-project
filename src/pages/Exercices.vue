3300e20f410cd67560b0775f023542856f46e986
Aude <aude@compani.fr>
2024-09-12 17:13:28 +0200

    try fetch api

--- src/pages/Exercices.vue
+++ src/pages/Exercices.vue
@@ -1,43 +1,38 @@
 <template>
-
-    <h1>Hello World !</h1>
-      <h2>{{resultData}}</h2>
-    <label for="category-select"> Choose a category</label>
-    <select name="category" id="category-select">
-      <option value="">--Please choose an option--</option>
-      <option value=""></option>
-    </select>
+  <h1>Hello World !</h1>
+  <h2>{{ resultData.title }}</h2>
+  <label for="category-select"> Choose a category</label>
+  <select name="category" id="category-select">
+    <option value="">--Please choose an option--</option>
+    <option value="recipes.title"></option>
+  </select>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
 
 export default {
   setup() {
+    const resultData = ref({});
 
-    const resultData = ref([]);
-    const apiKey = import.meta.env.API_KEY;
+    const fetchData = () => {
+       axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=f24ef45f93c94fafa322a61a9440cdb6`)
+        .then(response => (resultData.value = response.data.results));
+    };
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://api.api-ninjas.com/v1/exercises/', {
-        headers: {
-            'X-Api-Key': apiKey,
-            'Content-Type': 'application/json'
-          }})
-        console.log(response.data.result, 'toto')
-        resultData.value = response.data.result
-      } catch(error) {
-        console.log('Erreur lors de la requete', error)
-      }
-     
-    }
-    
-    fetchData();
+    console.log(resultData, 'toto')
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    const recipes = computed(() => resultData.value.map((recipe) => recipe._rawValue.id))
+    console.log(recipes, 'tototototo')
 
     return {
-      resultData
+      resultData,
+      recipes,
     };
   },
 };
